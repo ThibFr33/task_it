3b8725b813f1801be491df14208898b4fe6b12b4
Thibault Charron <thibs.cha@gmail.com>
2024-11-26 14:56:49 +0100

    création lists#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :dashboard, only: [:index]
+  resources :dashboards, only: [:index]
   resources :lists do
     resources :tasks, only: [:create, :update, :destroy]
     post 'ocr', on: :member
   end
   resources :reminders, only: [:new, :index, :update, :destroy] do
-    post 'ocr', on: :member
+    post 'ocr', on: :collection
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
