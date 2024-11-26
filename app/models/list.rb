dd3b7b1c5087f54edaddb8d56d790ec9a1339122
Morgane Bourdillon <mlacroix8@gmail.com>
2024-11-26 17:41:35 +0100

    change seed capitalize

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ApplicationRecord
-  enum category: { autre: 0, courses: 1, vacances: 2, maison: 3, administratif: 4, enfatns: 5 }
+  enum category: { autre: 0, courses: 1, vacances: 2, maison: 3, administratif: 4, enfants: 5 }
 
   belongs_to :user
   has_many :tasks
