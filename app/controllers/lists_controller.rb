a74877dcfc27704e6a9f65523595a1dc8b821854
Morgane Bourdillon <mlacroix8@gmail.com>
2024-11-27 17:30:40 +0100

    modal

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,4 +8,18 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+
+  def create
+    # raise
+    @list = List.new(list_params)
+    @list.user = current_user
+    @list.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :category)
+  end
 end
