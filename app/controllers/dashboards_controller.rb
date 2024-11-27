1a2ced31620e83e588ddd1bbcdc4bcfcb86e168f
Donovan Trigant <donovan.trigant@gmail.com>
2024-11-27 17:52:59 +0100

    homepage almost done

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,7 @@
 class DashboardsController < ApplicationController
   def index
+    @last_lists = List.last(3)
+    @reminders = Reminder.all
   end
   #Ex:- add_index("admin_users", "username")
 end
