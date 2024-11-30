c588d563c71e13703311c38d1c66c1d82b978c2e
Morgane Bourdillon <mlacroix8@gmail.com>
2024-11-30 14:40:38 +0100

    methode vision

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 require "openai"
+require "json"
 
 class ListsController < ApplicationController
 
@@ -32,35 +33,60 @@ class ListsController < ApplicationController
     redirect_to list_paths, status: :see_other
   end
 
-  def ocr(base64_image)
+  def ocr
+    @photo = encode_image
     client = OpenAI::Client.new
-    client.chat.completions.create(
-          model="gpt-4-vision-preview",
-          messages=[
-            {
-              "role": "user",
-              "content": [
-                {
-                  "type": "text",
-                  "text": "Je t'écris en français tu me réponds en français.
-                 Je t'envoies la photo d'une to do list. La photo est celle d'un document papier avec une écriture à la main.
-                Tu extraits chaque item séparemment. Si tu n'es pas sûr d'un item trouve la correspondance française la plus proche.",
-                },
-                {
-                  "type": "image_url",
-                  "image_url": {
-                    "url":  f"data:image/jpeg;base64,{base64_image}"
-                  },
-                },
-              ],
-            }
-          ],
-        )
+    messages = [
+      { "type": "text", "text": "Je t'écris en français tu me réponds en français.
+                                 Je t'envoies la photo d'une to do list.
+                                 La photo est celle d'un document papier avec une écriture à la main.
+                                 Tu extraits chaque item séparemment. Si tu n'es pas sûr d'un item trouve la
+                                 correspondance française la plus proche."},
+      { "type": "image_url",
+        "image_url": {
+          "url": "data:image/jpeg;base64,#{@photo}",
+        },
+      }
+    ]
+
+    response = client.chat(
+      parameters: {
+        model: "gpt-4o-mini", # Required.
+        response_format: "json",
+        messages: [
+          { role: "user", content: messages }
+        ] # Required.
+      }
+    )
+    raise
+    response
+    # client.chat.completions.create(
+    #       model="gpt-4-vision-preview",
+    #       messages=[
+    #         {
+    #           "role": "user",
+    #           "content": [
+    #             {
+    #               "type": "text",
+    #               "text": "Je t'écris en français tu me réponds en français.
+    #              Je t'envoies la photo d'une to do list. La photo est celle d'un document papier avec une écriture à la main.
+    #             Tu extraits chaque item séparemment. Si tu n'es pas sûr d'un item trouve la correspondance française la plus proche.",
+    #             },
+    #             {
+    #               "type": "image_url",
+    #               "image_url": {
+    #                 "url":  f"data:image/jpeg;base64,{base64_image}"
+    #               },
+    #             },
+    #           ],
+    #         }
+    #       ],
+    #     )
         return response.choices[0]
   end
-  
+
   def encode_image
-    file_content = File.read(params[:temp_photo].tempfile)
+    file_content = File.read(params[:ocr][:temp_photo].tempfile)
     Base64.strict_encode64(file_content)
   end
 
