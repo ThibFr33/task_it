9e6108822615ac9b9363f2edc6f5fb4f58cfdff3
Morgane Bourdillon <mlacroix8@gmail.com>
2024-12-06 08:58:06 +0100

    request Submit ciao la popup

--- app/javascript/controllers/submit_photo_controller.js
+++ app/javascript/controllers/submit_photo_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="submit-photo"
 export default class extends Controller {
   submitFormPhoto(event) {
-    this.element.submit()
+    this.element.requestSubmit()
   }
 }
